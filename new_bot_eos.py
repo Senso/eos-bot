cf9fdd91b9748516d6911fe5e48be43f754fd9d4
Sensoz <sensoz@gmail.com>
2012-04-12 15:42:52 +0700

    removed unused section

--- new_bot_eos.py
+++ new_bot_eos.py
@@ -148,16 +148,7 @@ class Web:
 			if div['class'] == 'prod_choices_item':
 				sub = div.contents[0].contents
 				for a in sub:
-					if a.name == 'a':
-						if a['class'] == u'load_in_fbox':
-							self.stores[store_id][a.contents[0]['title'].strip()] = {}
-						else:
-							if a.contents[0]['title'] == 'Selling':
-								self.stores[store_id][a.contents[1].strip()]['units'] = a.contents[1].strip()
-							elif a.contents[0]['title'] == 'Average Sales Price':
-								self.stores[store_id][a.contents[1].strip()]['price'] = a.contents[1].strip()
-								
-					elif a.name == 'div':
+					if a.name == 'div':
 						self.parse_outofstock(div.__str__())
 
 
