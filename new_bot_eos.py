453ec46ff7d8699cecb9b313f9b4d41048fb0220
Senso <sensoz@gmail.com>
2012-04-12 19:16:24 +0700

    setting the prices for real

--- new_bot_eos.py
+++ new_bot_eos.py
@@ -102,13 +102,23 @@ class Web:
 			print '\t\t\tCannot find average price.'
 			return
 		
-		
-		print '\t\t\twhid:', whid
-		print '\t\t\tCost:', str(cost)
-		print '\t\t\tAvg price:', str(avg_price)
+		selling_price = avg_price - (avg_price * 0.01)
+		if selling_price > cost:
+			# Good bet, we should make at least a quick buck
+		else:
+			# cost is higher than 1% off average price, try to sell at 5% markup
+			selling_price += selling_price * 0.05
 			
-
+		# We're dealing in cents
+		selling_price = selling_price * 100.0
 		
+		#print '\t\t\twhid:', whid
+		#print '\t\t\tCost:', str(cost)
+		#print '\t\t\tAvg price:', str(avg_price)
+		
+		# DO IT
+		resp = self.read_page(self.conf['urls']['set_price'] % (selling_price, whid))
+		print 'Tried to set price, got:', resp
 		
 	def buy_product(self, content, prod_name):
 		soup = BeautifulSoup(content)
