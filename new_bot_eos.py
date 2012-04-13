ab58154726ec0fad3a66cce93a0f45729b7013c7
Sensoz <sensoz@gmail.com>
2012-04-13 10:32:53 +0700

    added time to logfile

--- new_bot_eos.py
+++ new_bot_eos.py
@@ -40,7 +40,7 @@ class Log:
 		self.logger = logging.getLogger()
 		self.logger.setLevel(logging.INFO)
 		fhandler = FileHandler(filename=fname)
-		formatter = logging.Formatter('%(message)s')
+		formatter = logging.Formatter('%(asctime)-15s %(message)s')
 		fhandler.setFormatter(formatter)
 		self.logger.addHandler(fhandler)
 
